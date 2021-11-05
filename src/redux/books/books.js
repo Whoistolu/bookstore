040ad64163667252cbff541cda118150a4a85eaa
Whoistolu <digittolu25@gmail.com>
2021-11-05 22:06:18 +0100

    Reducer and export it as default

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -10,3 +10,15 @@ export const removeBook = (payload) => ({
   type: REMOVE_BOOK,
   payload,
 });
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export default reducer;

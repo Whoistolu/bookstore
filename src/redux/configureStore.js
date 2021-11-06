be5149a03272c9357ba48d0451939c14e38b5e80
Whoistolu <digittolu25@gmail.com>
2021-11-06 18:35:46 +0100

    Fix eslint-disable

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,7 @@
-/* eslint-disable */
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import booksReducer from './books/books';
-import { initializeBooks } from './books/books';
+import booksReducer, { initializeBooks } from './books/books';
 
 const reducer = combineReducers({
   booksReducer,

e047c9af35e6cefbdcf6bd42529ca7b544a46d05
Whoistolu <digittolu25@gmail.com>
2021-11-06 18:38:18 +0100

    Fix the configurestore spell check

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
-import store from './redux/cofigureStore';
+import store from './redux/configureStore';
 import './index.css';
 
 ReactDOM.render(
